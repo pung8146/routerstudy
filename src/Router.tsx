8a070837c9b305ddc8f80d13c21b0738fbdf9dec
pung8146 <pung8146@naver.com>
2023-05-03 19:00:51 +0900

    browserRouter사용하기

---
+++ src/Router.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./screens/Home";
+import About from "./screens/About";
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path={"/"} element={<Home />} />
+        <Route path={"/about"} element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
